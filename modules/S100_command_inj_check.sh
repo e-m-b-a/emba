4cc6084abd2e280fb82318a5d4c22c60f0239d0d
p4cx <p4cx@pm.me>
2021-01-14 15:16:20 -0800

    Update headers

--- modules/S100_command_inj_check.sh
+++ modules/S100_command_inj_check.sh
@@ -2,7 +2,7 @@
 
 # emba - EMBEDDED LINUX ANALYZER
 #
-# Copyright 2020 Siemens AG
+# Copyright 2020-2021 Siemens AG
 #
 # emba comes with ABSOLUTELY NO WARRANTY. This is free software, and you are
 # welcome to redistribute it under the terms of the GNU General Public License.
