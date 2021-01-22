268756082a77588e590b01804ad3dd5caee4bd17
m-1-k-3 <m1k3@s3cur1ty.de>
2021-01-22 13:31:31 +0100

    moved bc to the default tools

--- installer.sh
+++ installer.sh
@@ -133,6 +133,7 @@ print_tool_info "unzip" 1
 print_tool_info "docker-compose" 1
 print_tool_info "qemu-user-static" 0 "qemu-mips-static"
 print_tool_info "binwalk" 0
+print_tool_info "bc" 1
 
 echo -e "\\n""$MAGENTA""$BOLD""Do you want to install/update these applications?""$NC"
 read -p "(y/N)" -r ANSWER
@@ -313,10 +314,9 @@ esac
 INSTALL_APP_LIST=()
 echo -e "\\nTo use the aggregator and check if exploits are available, we need a searchable exploit database. CVE-searchsploit will be installed via pip3."
 print_tool_info "python3-pip" 1
-print_tool_info "bc" 1
 print_tool_info "net-tools" 1
 
-echo -e "\\n""$MAGENTA""$BOLD""Do you want to download and install bc, net-tools, pip3, cve-search and cve_searchsploit (if not already on the system)?""$NC"
+echo -e "\\n""$MAGENTA""$BOLD""Do you want to download and install the net-tools, pip3, cve-search and cve_searchsploit (if not already on the system)?""$NC"
 read -p "(y/N)" -r ANSWER
 case ${ANSWER:0:1} in
   y|Y )
