8b7da706e9492605ca6e2eda75fe2d999bc0344e
Stefan <haboeck.st@gmx.at>
2021-02-23 01:59:57 -0800

    reduce dependencies

--- helpers/html_converter.sh
+++ helpers/html_converter.sh
@@ -71,8 +71,8 @@ build_index_file(){
   if [[ -n ${INDEX_CONTENT_ARR[*]} ]]; then
     for OUTPUT in "${INDEX_CONTENT_ARR[@]}"; do
       if [[ "$OUTPUT" == *"entropy.png"* ]]; then
-        OUTPUT=${OUTPUT//"33m"/"33m <br> <img id=\"entropypic\" heigth=\"380px\" width=\"540px\" src=\" "}
-        OUTPUT="${OUTPUT:0:${#OUTPUT}-3}"" \">"
+        IFS=':' read -r -a "FILE_LINE_ARR" <<< "$OUTPUT"
+        OUTPUT="${FILE_LINE_ARR[0]}""33m <br> <img id=\"entropypic\" heigth=\"380px\" width=\"540px\" src=\"./style/entropy.png\">"
       fi
      
       if [[ "$OUTPUT" == *"Kernel vulnerabilities"* ]]; then
@@ -91,7 +91,10 @@ build_index_file(){
         echo -e "<h2 id=""${FILENAMES[$i]}"">""$OUTPUT""</h2><br>" | tee -a "$ABS_HTML_PATH""/index.txt" >/dev/null
         i=$(( i+1 ))
       else
-        echo -e "$OUTPUT""<br>" | tee -a "$ABS_HTML_PATH""/index.txt" >/dev/null
+        if [[ "$OUTPUT" != *"entropy.png"* ]]; then
+          OUTPUT="<span  style=\"white-space: pre\">$OUTPUT</span>"
+        fi
+        echo "<br>$OUTPUT" | tee -a "$ABS_HTML_PATH""/index.txt" >/dev/null
       fi
     done
   fi
@@ -100,8 +103,8 @@ build_index_file(){
   rm "$ABS_HTML_PATH""/index.txt"
 
   sed -i 's/&lt;/</g; s/&quot;/"/g; s/&gt;/>/g; s/<pre>//g; s/<\/pre>//g' "$ABS_HTML_PATH""/index.html"
-  ESCAPED_CONFIG_DIR=${ABS_CONFIG_DIR_PATH//\//\\\/}
-  sed -i "s/<head>/<head><br><link rel=\"stylesheet\" href=\"$ESCAPED_CONFIG_DIR\/style.css\" type=\"text\/css\"\/>/g" "$ABS_HTML_PATH""/index.html"
+  ESCAPED_HTML_PATH=${ABS_HTML_PATH//\//\\\/}
+  sed -i "s/<head>/<head><br><link rel=\"stylesheet\" href=\".\/style\/style.css\" type=\"text\/css\"\/>/g" "$ABS_HTML_PATH""/index.html"
 }
 
 
@@ -117,7 +120,6 @@ build_collection_file(){
   HTML_FILE="${FILE%.txt}.html"
   COLORLESS_FILE_LINE=$(head -1 "$FILE" | tail -n 1 | cut -c27-)
   LINKNAME="${COLORLESS_FILE_LINE// /_}"
-  NOT_FINDINGS_MENU_LIST+="<li><a href=\"./index.html\">Go back</a></li>"
   NOT_FINDINGS_MENU_LIST+="<li><a href=\"collection.html#$LINKNAME\">$COLORLESS_FILE_LINE</a></li>"
   if [ ${#NOT_FINDINGS_FILENAMES[@]} -ne 0 ]; then
     NOT_FINDINGS_FILENAMES[${#NOT_FINDINGS_FILENAMES[@]}]="$LINKNAME"
@@ -127,6 +129,7 @@ build_collection_file(){
 
   echo "$HTML_FILE_HEADER
     <ul>
+    <li><a href=\"./index.html\">Go back</a></li>
     $NOT_FINDINGS_MENU_LIST
     </ul>
     </div>
@@ -148,8 +151,8 @@ build_collection_file(){
   rm "$ABS_HTML_PATH""/collection.txt"
 
   sed -i 's/&lt;/</g; s/&quot;/"/g; s/&gt;/>/g; s/<pre>//g; s/<\/pre>//g' "$ABS_HTML_PATH""/collection.html"
-  ESCAPED_CONFIG_DIR=${ABS_CONFIG_DIR_PATH//\//\\\/}
-  sed -i "s/<head>/<head><br><link rel=\"stylesheet\" href=\"$ESCAPED_CONFIG_DIR\/style.css\" type=\"text\/css\"\/>/g" "$ABS_HTML_PATH""/collection.html"
+  ESCAPED_HTML_PATH=${ABS_HTML_PATH//\//\\\/}
+  sed -i "s/<head>/<head><br><link rel=\"stylesheet\" href=\".\/style\/style.css\" type=\"text\/css\"\/>/g" "$ABS_HTML_PATH""/collection.html"
 }
 
 build_report_files(){
@@ -183,16 +186,18 @@ build_report_files(){
     done
   fi
  
-  echo "<header><div class=\"pictureleft\"><img src=\"$ABS_CONFIG_DIR_PATH/emba.png\"></div>
-    <div class=\"headline\"><h1>$HEADLINE</h1></div><div class=\"pictureright\"><img src=\"$ABS_CONFIG_DIR_PATH/emba.png\">
+  echo "<header><div class=\"pictureleft\"><img width=\"100px\" heigth=\"100px\" src=\"./style/emba.svg\"></div>
+    <div class=\"headline\"><h1>$HEADLINE</h1></div><div class=\"pictureright\"><img width=\"100px\" heigth=\"100px\" src=\"./style/emba.svg\">
     </div></header><div><ul>$SUB_MENU_LIST</ul></div><div class=\"main\">" | tee -a "$ABS_HTML_PATH""/$FILENAME" >/dev/null
  
   TOP10_FORMAT_COUNTER=0
+  ESCAPED_HTML_PATH=${ABS_HTML_PATH//\//\\\/}
   if [[ -n ${REPORT_ARRAY[*]} ]]; then
     for FILE_LINE in "${REPORT_ARRAY[@]}"; do
       if [[ "$FILE_LINE" == *"entropy.png"* ]]; then
-        FILE_LINE=${FILE_LINE//"33m"/"33m <br> <img id=\"entropypic\" heigth=\"380px\" width=\"540px\" src=\" "}
-        FILE_LINE="${FILE_LINE:0:${#FILE_LINE}-3}"" \">"
+        IFS=':' read -r -a "FILE_LINE_ARR" <<< "$FILE_LINE"
+        cp "$LOG_DIR"/*entropy.png "$ABS_HTML_PATH""/style/entropy.png"
+        FILE_LINE="${FILE_LINE_ARR[0]}""33m <br> <img id=\"entropypic\" heigth=\"380px\" width=\"540px\" src=\"./style/entropy.png\">"
       fi
 	
       if [[ $FILE_LINE == *"[[0;34m+[0m] [0;36m[1m"* ]]; then
@@ -204,7 +209,10 @@ build_report_files(){
         echo "<h4 id=""${COLORLESS_FILE_LINE// /_}"">$FILE_LINE</h4>" | tee -a "$ABS_HTML_PATH""/$FILENAME" >/dev/null
         SUB_MENU_LIST="$SUB_MENU_LIST<li><a href=\"""$HTML_FILE#${COLORLESS_FILE_LINE// /_}""\">""$COLORLESS_FILE_LINE""</a></li>"
       else
-        echo "<br> <span  style=\"white-space: pre\">$FILE_LINE</span>" | tee -a "$ABS_HTML_PATH""/$FILENAME" >/dev/null
+        if [[ "$FILE_LINE" != *"entropy.png"* ]]; then
+          FILE_LINE="<span  style=\"white-space: pre\">$FILE_LINE</span>"
+        fi
+        echo "<br>$FILE_LINE" | tee -a "$ABS_HTML_PATH""/$FILENAME" >/dev/null
       fi
     done
   fi
@@ -213,24 +221,28 @@ build_report_files(){
   rm "$ABS_HTML_PATH""/$FILENAME"
   
   sed -i 's/&lt;/</g; s/&gt;/>/g; s/&quot;/"/g; s/<pre>//g; s/<\/pre>//g' "$ABS_HTML_PATH""/$HTML_FILE"
-  ESCAPED_CONFIG_DIR=${ABS_CONFIG_DIR_PATH//\//\\\/}
   ESCAPED_SUB_MENU_LIST=${SUB_MENU_LIST//\//\\\/}
   sed -i "s/<ul><\/ul>/<ul>$ESCAPED_SUB_MENU_LIST<\/ul>/g" "$ABS_HTML_PATH""/$HTML_FILE"
-  sed -i "s/<head>/<head><br><link rel=\"stylesheet\" href=\"$ESCAPED_CONFIG_DIR\/style.css\" type=\"text\/css\"\/>/g" "$ABS_HTML_PATH""/$HTML_FILE"
+  sed -i "s/<head>/<head><br><link rel=\"stylesheet\" href=\".\/style\/style.css\" type=\"text\/css\"\/>/g" "$ABS_HTML_PATH""/$HTML_FILE"
 }
 
 generate_html_file(){
   
-  ABS_CONFIG_DIR_PATH="$(abs_path "$CONFIG_DIR")"
   ABS_HTML_PATH="$(abs_path "$HTML_PATH")"
   
+  if [ ! -d "$ABS_HTML_PATH/style" ] ; then
+    mkdir "$ABS_HTML_PATH/style"
+    cp "$HELP_DIR/style.css" "$ABS_HTML_PATH/style/style.css"
+    cp "$HELP_DIR/emba.svg" "$ABS_HTML_PATH/style/emba.svg"
+  fi
+
   HTML_FILE_HEADER="<header>
-    <div class=\"pictureleft\"><img src=\"$ABS_CONFIG_DIR_PATH/emba.png\"></div>
+    <div class=\"pictureleft\"><img width=\"100px\" heigth=\"100px\" src=\"./style/emba.svg\"></div>
     <div class=\"headline\">
     <h1>emba Report Manager</h1> 
     </div>
     <div class=\"pictureright\">
-    <img src=\"$ABS_CONFIG_DIR_PATH/emba.png\">
+    <img width=\"100px\" heigth=\"100px\" src=\"./style/emba.svg\">
     </div>
     </header>"
 
