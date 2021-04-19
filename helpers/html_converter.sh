0dd7a385aeb5e2794f729bd64a3208e728cb2724
Stefan <haboeck.st@gmx.at>
2021-04-19 07:58:05 +0200

    format Update Part 1/2

--- helpers/html_converter.sh
+++ helpers/html_converter.sh
@@ -14,17 +14,37 @@
 
 declare -a MENU_LIST
 
-add_aggegator_links(){
+add_aggegator_links_to_numbers(){
   OUTPUT=$1
   LINKED_FILE=$2
   IFS=' ' read -r -a "FILE_LINE_ARR" <<< "$OUTPUT"
   OUTPUT=""
   if [[ -n ${FILE_LINE_ARR[*]} ]]; then
     for WORDS in "${FILE_LINE_ARR[@]}"; do
-      if [[ "$WORDS" == *"[0;33m"* ]]; then
-        OUTPUT="$OUTPUT""<a href=\"""$LINKED_FILE""\">""$WORDS""</a>"
+    <\/a>\[0;32m
+      if [[ "$WORDS" == *"[0;33m"* ]] && [[ "$OUTPUT" == *"[0;32m"* ]]; then
+        OUTPUT="$OUTPUT""<a href=\"""$LINKED_FILE""\">""$WORDS""</a>[0;32m"
+      elif [[ "$WORDS" == *"[0;33m"* ]]; then
+        OUTPUT="$OUTPUT""<a href=\"""$LINKED_FILE""\">""$WORDS""</a> "
       else
-        OUTPUT="$OUTPUT""$WORDS"
+        OUTPUT="$OUTPUT""$WORDS"" "
+      fi
+    done
+  fi
+}
+
+add_aggegator_links_to_strings(){
+  OUTPUT=$1
+  LINKED_FILE=$2
+
+  IFS=' ' read -r -a "FILE_LINE_ARR" <<< "$OUTPUT"
+  OUTPUT=""
+  if [[ -n ${FILE_LINE_ARR[*]} ]]; then
+    for WORDS in "${FILE_LINE_ARR[@]}"; do
+      if [[ "$WORDS" == *"[0;33m"* ]]; then
+        OUTPUT="$OUTPUT""<a href=\"""$LINKED_FILE""\">""$WORDS""</a> "
+      else
+        OUTPUT="$OUTPUT""$WORDS"" "
       fi
     done
   fi
@@ -90,35 +110,39 @@ build_index_file(){
       fi
 
       if [[ "$OUTPUT" == *"files and"*"directories detected."* ]]; then
-        add_aggegator_links "$OUTPUT" "s05_firmware_details.html"
+        add_aggegator_links_to_numbers "$OUTPUT" "s05_firmware_details.html"
       elif [[ "$OUTPUT" == *"Found"*"issues in"*"shell scripts."* ]]; then
-        add_aggegator_links "$OUTPUT" "s20_shell_check.html"
+        add_aggegator_links_to_numbers "$OUTPUT" "s20_shell_check.html"
       elif [[ "$OUTPUT" == *"Found"*"issues in"*"python scripts."* ]]; then
-        add_aggegator_links "$OUTPUT" "s21_python_check.html"
+        add_aggegator_links_to_numbers "$OUTPUT" "s21_python_check.html"
       elif [[ "$OUTPUT" == *"Found"*"yara rule matches in"*"files."* ]]; then
-        add_aggegator_links "$OUTPUT" "s110_yara_check.html"
+        add_aggegator_links_to_numbers "$OUTPUT" "s110_yara_check.html"
       elif [[ "$OUTPUT" == *"Found"*"areas with weak permissions."* ]]; then
-        add_aggegator_links "$OUTPUT" "s40_weak_perm_check.html"
+        add_aggegator_links_to_numbers "$OUTPUT" "s40_weak_perm_check.html"
+        OUTPUT="    ""$OUTPUT"
       elif [[ "$OUTPUT" == *"Found passwords or weak credential configuration - check log file for details."* ]]; then
         OUTPUT="<a href=\"s45_pass_file_check.html\">""$OUTPUT""</a>"
       elif [[ "$OUTPUT" == *"Found"*"kernel modules with"*"licensing issues."* ]]; then
-        add_aggegator_links "$OUTPUT" "s25_kernel_check.html#Analyze_kernel_modules"
+        add_aggegator_links_to_numbers "$OUTPUT" "s25_kernel_check.html#Analyze_kernel_modules"
+        OUTPUT="    ""$OUTPUT"
       elif [[ "$OUTPUT" == *"Found"*"not common Linux files with"*"files at all."* ]]; then
-        add_aggegator_links "$OUTPUT" "s108_linux_common_file_checker.html"
+        add_aggegator_links_to_numbers "$OUTPUT" "s108_linux_common_file_checker.html"
+        OUTPUT="    ""$OUTPUT"
       elif [[ "$OUTPUT" == *"Found"*"interesting files and"*"files that could be useful for post-exploitation."* ]]; then
-        add_aggegator_links "$OUTPUT" "s95_interesting_binaries_check.html"
+        add_aggegator_links_to_numbers "$OUTPUT" "s95_interesting_binaries_check.html"
+        OUTPUT="    ""$OUTPUT"
       elif [[ "$OUTPUT" == *"Found"*"binaries without enabled stack canaries in"*"binaries."* ]]; then
-        add_aggegator_links "$OUTPUT" "s10_binaries_check.html#Binary_check_for_mechanisms_via_checksec.sh"
+        add_aggegator_links_to_numbers "$OUTPUT" "s10_binaries_check.html#Binary_check_for_mechanisms_via_checksec.sh"
       elif [[ "$OUTPUT" == *"Found"*"binaries without enabled RELRO in"*"binaries."* ]]; then
-        add_aggegator_links "$OUTPUT" "s10_binaries_check.html#Binary_check_for_mechanisms_via_checksec.sh"
+        add_aggegator_links_to_numbers "$OUTPUT" "s10_binaries_check.html#Binary_check_for_mechanisms_via_checksec.sh"
       elif [[ "$OUTPUT" == *"Found"*"binaries without enabled NX in"*"binaries."* ]]; then
-        add_aggegator_links "$OUTPUT" "s10_binaries_check.html#Binary_check_for_mechanisms_via_checksec.sh"
+        add_aggegator_links_to_numbers "$OUTPUT" "s10_binaries_check.html#Binary_check_for_mechanisms_via_checksec.sh"
       elif [[ "$OUTPUT" == *"Found "*"binaries without enabled PIE in"*"binaries."* ]]; then
-        add_aggegator_links "$OUTPUT" "s10_binaries_check.html#Binary_check_for_mechanisms_via_checksec.sh"
+        add_aggegator_links_to_numbers "$OUTPUT" "s10_binaries_check.html#Binary_check_for_mechanisms_via_checksec.sh"
       elif [[ "$OUTPUT" == *"Found "*"stripped binaries without symbols in "*" binaries"* ]]; then
-        add_aggegator_links "$OUTPUT" "s10_binaries_check.html#Binary_check_for_mechanisms_via_checksec.sh"
+        add_aggegator_links_to_numbers "$OUTPUT" "s10_binaries_check.html#Binary_check_for_mechanisms_via_checksec.sh"
       elif [[ "$OUTPUT" == *"Found"*"usages of strcpy in"*"binaries. "* ]]; then
-        add_aggegator_links "$OUTPUT" "s10_binaries_check.html#Generating_objdump_disassembly"
+        add_aggegator_links_to_numbers "$OUTPUT" "s10_binaries_check.html#Generating_objdump_disassembly"
       elif [[ "$OUTPUT" == *"Found version details:"* ]]; then
         OUTPUT="<a href=\"s09_firmware_base_version_check.html\">""$OUTPUT""</a>"
       fi
@@ -135,7 +159,7 @@ build_index_file(){
         TOP10_FORMAT_COUNTER=0
       elif [ "$TOP10_FORMAT_COUNTER" -ne 0 ]; then
         TOP10_FORMAT_COUNTER=$(( TOP10_FORMAT_COUNTER+1 ))
-        OUTPUT="<span  style=\"white-space: pre\">""<a href=\"s10_binaries_check.html#Generating_objdump_disassembly\">""$OUTPUT""</a>""</span>"
+        OUTPUT="<a href=\"s10_binaries_check.html#Generating_objdump_disassembly\">""<span  style=\"white-space: pre\">""$OUTPUT""</span>""</a>"
       fi
 	
       if [[ "$OUTPUT" == *"0;34m+"*"0;36m"* ]] && [[ "$OUTPUT" != *"h2 id"* ]]; then
