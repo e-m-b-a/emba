18271464f45cfcdf247c15159c3895324e1b4049
m-1-k-3 <michael.messner@siemens-energy.com>
2021-06-23 11:03:45 +0200

    cwe checker link

--- modules/F50_base_aggregator.sh
+++ modules/F50_base_aggregator.sh
@@ -611,16 +611,16 @@ cwe_logging() {
   if [[ -d "$LOG_DIR"/"$LOG_DIR_MOD" ]]; then
     mapfile -t CWE_OUT < <( cat "$LOG_DIR"/"$LOG_DIR_MOD"/cwe_*.log 2>/dev/null | grep -v "ERROR\|DEBUG\|INFO" | grep "CWE[0-9]" | sed -z 's/[0-9]\.[0-9]//g' | cut -d\( -f1,3 | cut -d\) -f1 | sort -u | tr -d '(' | tr -d "[" | tr -d "]" )
     if [[ ${#CWE_OUT[@]} -gt 0 ]] ; then
-      print_output "[+] cwe-checker found the following security issues:"
+      print_output "[+] cwe-checker found a total of ""$ORANGE""$TOTAL_CWE_CNT""$GREEN"" of the following security issues:"
+      write_link "s120"
       for CWE_LINE in "${CWE_OUT[@]}"; do
         CWE="$(echo "$CWE_LINE" | cut -d\  -f1)"
         CWE_DESC="$(echo "$CWE_LINE" | cut -d\  -f2-)"
         CWE_CNT="$(cat "$LOG_DIR"/"$LOG_DIR_MOD"/cwe_*.log 2>/dev/null | grep -c "$CWE")"
         print_output "$(indent "$(orange "$CWE""$GREEN"" - ""$CWE_DESC"" - ""$ORANGE""$CWE_CNT"" times.")")"
-        (( CWE_CNT_FULL= "$CWE_CNT_FULL" + "$CWE_CNT" ))
       done
-      print_output "[+] cwe-checker found a total of $CWE_CNT_FULL security issues."
-      echo "cwe_issues;\"$CWE_CNT_FULL\"" >> "$CSV_LOG_FILE"
+      print_output ""
+      echo "cwe_issues;\"$TOTAL_CWE_CNT\"" >> "$CSV_LOG_FILE"
     fi
   fi
 }
--- modules/S120_cwe_checker.sh
+++ modules/S120_cwe_checker.sh
@@ -91,6 +91,7 @@ final_cwe_log() {
         CWE_CNT="$(cat "$LOG_PATH_MODULE"/cwe_*.log 2>/dev/null | grep -c "$CWE")"
         print_output "$(indent "$(orange "$CWE""$GREEN"" - ""$CWE_DESC"" - ""$ORANGE""$CWE_CNT"" times.")")"
       done
+      print_output ""
     fi
   fi
 }
