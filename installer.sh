51aaa511ad54620d48381954e89449de17ca1d2e
m-1-k-3 <michael.messner@siemens-energy.com>
2021-08-04 17:28:35 +0200

    installer final cleanup - docker ok

--- installer.sh
+++ installer.sh
@@ -580,7 +580,7 @@ case ${ANSWER:0:1} in
       fi
       case ${ANSWER:0:1} in
         y|Y )
-          #cve_searchsploit -u
+          cve_searchsploit -u
         ;;
       esac    
     fi
@@ -597,7 +597,10 @@ print_tool_info "python3-pyqt5" 1
 print_tool_info "python3-pyqt5.qtopengl" 1
 print_tool_info "python3-numpy" 1
 print_tool_info "python3-scipy" 1
+# python2 is needed for ubireader installation
 print_tool_info "python2" 1
+# python-setuptools is needed for ubireader installation
+print_tool_info "python-setuptools" 1
 print_tool_info "mtd-utils" 1
 print_tool_info "gzip" 1
 print_tool_info "bzip2" 1
@@ -697,12 +700,13 @@ case ${ANSWER:0:1} in
       cd ./external/binwalk/ubi_reader || exit 1
       git reset --hard 0955e6b95f07d849a182125919a1f2b6790d5b51
       python2 setup.py install
-      cd .. || exit 1
+      cd ../../.. || exit 1
     else
       echo -e "$GREEN""ubi_reader already installed""$NC"
     fi
 
     if ! command -v binwalk > /dev/null ; then
+      cd ./external/binwalk
       python3 setup.py install
     else
       echo -e "$GREEN""binwalk already installed""$NC"
