aa09113ccb59ba6530924f258cb639ef7979caf8
m-1-k-3 <michael.messner@siemens-energy.com>
2021-09-02 15:10:19 +0200

    more docker run stuff

--- emba.sh
+++ emba.sh
@@ -262,6 +262,7 @@ main()
         export ONLY_DEP=1
         ;;
       D)
+        # new debugging mode
         export USE_DOCKER=0
         ;;
       e)
@@ -287,7 +288,9 @@ main()
         exit 0
         ;;
       i)
+        # for detecting the execution in docker container:
         export IN_DOCKER=1
+        export USE_DOCKER=0
         ;;
       k)
         export KERNEL=1
@@ -511,18 +514,18 @@ main()
 
     echo
 
-    if ! docker images | grep -qE "^emba[[:space:]]*latest"; then
+    if ! docker images | grep -qE "emba[[:space:]]*latest"; then
       print_output "[*] Emba sets up the docker environment.\\n" "no_log"
-      EMBA="$INVOCATION_PATH" FIRMWARE="$FIRMWARE_PATH" LOG="$LOG_DIR" docker-compose build emba
+      EMBA="$INVOCATION_PATH" FIRMWARE="$FIRMWARE_PATH" LOG="$LOG_DIR" docker pull embeddedanalyzer/emba
       DBUILD_RETURN=$?
 
-      if [[ $DBUILD_RETURN -ne 0 ]] ; then
+      if ! docker images | grep -qE "emba[[:space:]]*latest"; then
         print_output "[-] Emba docker build failed!" "no_log"
         exit 1
       fi
     fi
 
-    if docker images | grep -qE "^emba[[:space:]]*latest"; then
+    if docker images | grep -qE "emba[[:space:]]*latest"; then
       setup_docker_iptables
 
       print_output "[*] Emba initializes docker container.\\n" "no_log"
