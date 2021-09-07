fa6cfba77f30ea938a8db9ad8e0de26e4b5b94b5
m-1-k-3 <michael.messner@siemens-energy.com>
2021-09-07 17:22:13 +0200

    error redirection

--- installer.sh
+++ installer.sh
@@ -411,25 +411,24 @@ if [[ "$LIST_DEP" -eq 1 ]] || [[ $IN_DOCKER -eq 1 ]] || [[ $DOCKER_SETUP -eq 0 ]
       # Java SDK for ghidra
       if [[ -d .external/jdk ]] ; then rm -R .external/jdk ; fi
       curl -L https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.12%2B7/OpenJDK11U-jdk_x64_linux_hotspot_11.0.12_7.tar.gz -Sf -o external/jdk.tar.gz
-      mkdir external/jdk
+      mkdir external/jdk 2>/dev/null
       tar -xzf external/jdk.tar.gz -C external/jdk --strip-components 1
       rm external/jdk.tar.gz
   
       # Ghidra
       if [[ -d .external/ghidra ]] ; then rm -R .external/ghidra ; fi
       curl -L https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_10.0.2_build/ghidra_10.0.2_PUBLIC_20210804.zip -Sf -o external/ghidra.zip
-      mkdir external/ghidra
+      mkdir external/ghidra 2>/dev/null
       unzip -qo external/ghidra.zip -d external/ghidra
       sed -i s@JAVA_HOME_OVERRIDE=@JAVA_HOME_OVERRIDE=external/jdk@g external/ghidra/ghidra_10.0.2_PUBLIC/support/launch.properties
       rm external/ghidra.zip
   
       if [[ -d .external/cwe_checker ]] ; then rm -R .external/cwe_checker ; fi
-      mkdir external/cwe_checker
+      mkdir external/cwe_checker 2>/dev/null
       git clone https://github.com/fkie-cad/cwe_checker.git external/cwe_checker
       cd external/cwe_checker || exit 1
       make all GHIDRA_PATH=external/ghidra/ghidra_10.0.2_PUBLIC
       cd "$HOME_PATH" || exit 1
-      #cp -r "$HOME""/.cargo/bin" "external/cwe_checker/bin"
       mv "$HOME""/.cargo/bin" "external/cwe_checker/bin"
     ;;
   esac
