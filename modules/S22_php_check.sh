a7763fb7d9ba7c28d0c5a79497eefc2dd42a8327
m-1-k-3 <michael.messner@siemens-energy.com>
2021-10-19 14:27:57 +0200

    s22 cleanup

--- modules/S22_php_check.sh
+++ modules/S22_php_check.sh
@@ -19,7 +19,7 @@
 S22_php_check()
 {
   module_log_init "${FUNCNAME[0]}"
-  module_title "Check php scripts for vulnerabilities"
+  module_title "PHP vulnerability checks"
 
   S22_PHP_VULNS=0
   S22_PHP_SCRIPTS=0
@@ -28,30 +28,10 @@ S22_php_check()
 
   if [[ $PHP_CHECK -eq 1 ]] ; then
     mapfile -t PHP_SCRIPTS < <( find "$FIRMWARE_PATH" -xdev -type f -iname "*.php" -exec md5sum {} \; 2>/dev/null | sort -u -k1,1 | cut -d\  -f3 )
-    for LINE in "${PHP_SCRIPTS[@]}" ; do
-      if ( file "$LINE" | grep -q "PHP script" ) ; then
-        ((S22_PHP_SCRIPTS++))
-        if [[ "$THREADED" -eq 1 ]]; then
-          s22_vuln_check &
-          WAIT_PIDS_S22+=( "$!" )
-        else
-          s22_vuln_check
-        fi
-      fi
-    done
+    s22_vuln_check_caller
 
-    if [[ "$THREADED" -eq 1 ]]; then
-      wait_for_pid "${WAIT_PIDS_S22[@]}"
-    fi
-
-    if [[ -f "$TMP_DIR"/S22_VULNS.tmp ]]; then
-      while read -r VULNS; do
-        (( S22_PHP_VULNS="$S22_PHP_VULNS"+"$VULNS" ))
-      done < "$TMP_DIR"/S22_VULNS.tmp
-    fi
     s22_check_php_ini
-    print_output ""
-    print_output "[+] Found ""$ORANGE""$S22_PHP_VULNS"" vulnerabilities""$GREEN"" in ""$ORANGE""$S22_PHP_SCRIPTS""$GREEN"" php files.""$NC""\\n"
+
     write_log ""
     write_log "[*] Statistics:$S22_PHP_VULNS:$S22_PHP_SCRIPTS:$S22_PHP_INI_ISSUES:$S22_PHP_INI_CONFIGS"
 
@@ -59,6 +39,35 @@ S22_php_check()
     print_output "[-] PHP check is disabled ... no tests performed"
   fi
   module_end_log "${FUNCNAME[0]}" "$(( "$S22_PHP_VULNS" + "$S22_PHP_INI_ISSUES" ))"
+}
+
+s22_vuln_check_caller() {
+  sub_module_title "PHP script vulnerabilities"
+
+  for LINE in "${PHP_SCRIPTS[@]}" ; do
+    if ( file "$LINE" | grep -q "PHP script" ) ; then
+      ((S22_PHP_SCRIPTS++))
+      if [[ "$THREADED" -eq 1 ]]; then
+        s22_vuln_check &
+        WAIT_PIDS_S22+=( "$!" )
+      else
+        s22_vuln_check
+      fi
+    fi
+  done
+
+  if [[ "$THREADED" -eq 1 ]]; then
+    wait_for_pid "${WAIT_PIDS_S22[@]}"
+  fi
+
+  if [[ -f "$TMP_DIR"/S22_VULNS.tmp ]]; then
+    while read -r VULNS; do
+      (( S22_PHP_VULNS="$S22_PHP_VULNS"+"$VULNS" ))
+    done < "$TMP_DIR"/S22_VULNS.tmp
+  fi
+ 
+  print_output ""
+  print_output "[+] Found ""$ORANGE""$S22_PHP_VULNS"" vulnerabilities""$GREEN"" in ""$ORANGE""$S22_PHP_SCRIPTS""$GREEN"" php files.""$NC""\\n"
 }
 
 s22_vuln_check() {
@@ -106,28 +115,8 @@ s22_script_check() {
   fi
 }
 
-add_recommendations(){
-   local VALUE
-   local KEY
-   VALUE="$1"
-   KEY="$2"
-
-   if [[ $VALUE == *"M"* ]]; then
-      LIMIT="${VALUE//M/}"
-   fi
-
-   if [[ $KEY == *"memory_limit"* ]] && [[ $(( LIMIT)) -gt 50 ]]; then
-     return 1
-   elif [[ $KEY == *"post_max_size"* ]] && [[ $(( LIMIT)) -gt 20 ]]; then
-     return 1
-   elif [[ $KEY == *"max_execution_time"* ]] && [[ $(( LIMIT )) -gt 60 ]]; then
-     return 1
-   else
-     return 0
-   fi
-}
-
 s22_check_php_ini(){
+  sub_module_title "PHP configuration checks"
   local PHP_INI_FAILURE
   local PHP_INI_LIMIT_EXCEEDED
   local PHP_INI_WARNINGS
@@ -136,7 +125,7 @@ s22_check_php_ini(){
   PHP_INI_WARNINGS=0
   mapfile -t PHP_INI_FILE < <( find "$FIRMWARE_PATH" -xdev "${EXCL_FIND[@]}" -iname 'php.ini' -exec md5sum {} \; 2>/dev/null | sort -u -k1,1 | cut -d\  -f3 )
   for PHP_FILE in "${PHP_INI_FILE[@]}" ;  do
-    print_output "[*] iniscan check of ""$(print_path "$PHP_FILE")"
+    #print_output "[*] iniscan check of ""$(print_path "$PHP_FILE")"
     mapfile -t INISCAN_RESULT < <( "$PHP_INISCAN_PATH" scan --path="$PHP_FILE" 2>/dev/null)
     for LINE in "${INISCAN_RESULT[@]}" ; do  
       local LIMIT_CHECK
@@ -167,5 +156,31 @@ s22_check_php_ini(){
         fi
       fi
     done
+    print_output ""
+    print_output "[+] Found ""$ORANGE""$S22_PHP_INI_ISSUES""$GREEN"" PHP configuration issues in php config file :""$ORANGE"" ""$(print_path "$PHP_FILE")"
+    print_output ""
   done
 }
+
+add_recommendations(){
+   local VALUE
+   local KEY
+   VALUE="$1"
+   KEY="$2"
+
+   if [[ $VALUE == *"M"* ]]; then
+      LIMIT="${VALUE//M/}"
+   fi
+
+   if [[ $KEY == *"memory_limit"* ]] && [[ $(( LIMIT)) -gt 50 ]]; then
+     return 1
+   elif [[ $KEY == *"post_max_size"* ]] && [[ $(( LIMIT)) -gt 20 ]]; then
+     return 1
+   elif [[ $KEY == *"max_execution_time"* ]] && [[ $(( LIMIT )) -gt 60 ]]; then
+     return 1
+   else
+     return 0
+   fi
+}
+
+
