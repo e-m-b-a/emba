170a79d48f4b887b4e330dfc5a633c7ed0ef01e6
BenediktMKuehne <bennimail.4spam@gmail.com>
2021-11-30 09:07:03 +0100

    add redis Password to conf

--- installer.sh
+++ installer.sh
@@ -1228,6 +1228,7 @@ if [[ "$LIST_DEP" -eq 1 ]] || [[ $IN_DOCKER -eq 1 ]] || [[ $DOCKER_SETUP -eq 1 ]
     sed -i 's/127.0.0.1/172.36.0.1/g' ./etc/configuration.ini
     sed -zE 's/172.36.0.1([^\n]*\n[^\n]*6379)/127.0.0.1\1/' ./etc/configuration.ini | tee ./etc/configuration.ini.new
     cp ./etc/configuration.ini.new ./etc/configuration.ini  # This excludes redis for the installation
+    sed -i 's/^\#\ requirepass\ foobared/requirepass\ RedisPassword/g' /etc/redis/redis.conf
   fi
    
   case ${ANSWER:0:1} in
