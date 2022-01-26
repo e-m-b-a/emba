abb8afe91445a8d304cb9d533745b2174f7d49f9
m-1-k-3 <m1k3@s3cur1ty.de>
2022-01-26 10:37:22 +0100

    more templates

---
+++ report_templates/P16_EnGenius_decryptor-post.sh
@@ -0,0 +1,3 @@
+print_output "EMBA was able to identify an encrypted ${ORANGE}EnGenius${NC} firmware image. This firmware image was protected with leaked key material and it is possible to decrypt the firmware for further analysis."
+print_output ""
+print_output "The original firmware was decrypted to ${ORANGE}$EXTRACTION_FILE${NC}"
