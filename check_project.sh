d64dc0c9c193a88fbbab7d98297488b41ba2bc19
m-1-k-3 <m1k3@s3cur1ty.de>
2022-02-01 13:09:20 +0100

    wickStrictMode for check_project

--- check_project.sh
+++ check_project.sh
@@ -15,6 +15,22 @@
 
 # Description:  Check all shell scripts inside ./helpers, ./modules, emba.sh and itself with shellchecker
 
+STRICT=1
+
+if [[ "$STRICT" -eq 1 ]]; then
+  # shellcheck disable=SC1091
+  source ./installer/wickStrictModeFail.sh
+  # http://redsymbol.net/articles/unofficial-bash-strict-mode/
+  # https://github.com/tests-always-included/wick/blob/master/doc/bash-strict-mode.md
+  set -e          # Exit immediately if a command exits with a non-zero status
+  set -u          # Exit and trigger the ERR trap when accessing an unset variable
+  set -o pipefail # The return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status
+  set -E          # The ERR trap is inherited by shell functions, command substitutions and commands in subshells
+  shopt -s extdebug # Enable extended debugging
+  IFS=$'\n\t'     # Set the "internal field separator"
+  trap 'wickStrictModeFail $?' ERR  # The ERR trap is triggered when a script catches an error
+fi
+
 GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 BOLD='\033[1m'
