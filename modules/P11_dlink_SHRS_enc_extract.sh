540d559d5472abfdca546b4a7bce88832cd597ca
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-03-10 06:04:53 -0500

    ch expr style

--- modules/P11_dlink_SHRS_enc_extract.sh
+++ modules/P11_dlink_SHRS_enc_extract.sh
@@ -75,9 +75,9 @@ dlink_enc_img_extr(){
   dd if="$DLINK_ENC_PATH_" skip=16 iflag=skip_bytes of="$TMP_IMAGE_FILE" 2>&1 | tee -a "$LOG_FILE"
 
   IMAGE_SIZE=$(stat -c%s "$TMP_IMAGE_FILE")
-  let "ITERATIONS = $IMAGE_SIZE / 131072"
-  for ((ITERATION=0; ITERATION<$ITERATIONS; ITERATION++)); do
-    let "OFFSET = 131072 * $ITERATION"
+  (( ROOF=IMAGE_SIZE/131072 ))
+  for ((ITERATION=0; ITERATION<ROOF; ITERATION++)); do
+    (( OFFSET=131072*ITERATION ))
     dd if="$TMP_IMAGE_FILE" skip="$OFFSET" iflag=skip_bytes count=256| openssl aes-256-cbc -d -in /dev/stdin  -out /dev/stdout \
     -K "6865392d342b4d212964363d6d7e7765312c7132613364316e26322a5a5e2538" -iv "4a253169516c38243d6c6d2d3b384145" --nopad \
     --nosalt | dd if=/dev/stdin of="$EXTRACTION_FILE_" oflag=append conv=notrunc 2>&1 | tee -a "$LOG_FILE"
