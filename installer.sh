c3fa91097a10755a17d36078ab2aec519f268e1e
m-1-k-3 <m1k3@s3cur1ty.de>
2022-03-30 09:15:26 +0200

    full system emulation installer

--- installer.sh
+++ installer.sh
@@ -179,10 +179,15 @@ if [[ "$CVE_SEARCH" -ne 1 ]] || [[ "$DOCKER_SETUP" -ne 1 ]] || [[ "$IN_DOCKER" -
 
   I120_cwe_checker
 
+  # deprecated:
   IL10_system_emulator
+
   # for the future
-  IL20_system_emulator
+  #IL20_system_emulator
+
+  # deprecated:
   IL21_firmae_system_emulator
+  # deprecated:
   IL22_firmadyne_system_emulator
 
   IL15_emulated_checks_init
