6583823d7cc9235cdd91fcfb6903739f022818af
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-06-27 08:45:41 +0200

    add deb libssl1.1

--- installer/I02_custom_os.sh
+++ installer/I02_custom_os.sh
@@ -13,9 +13,18 @@
 #
 # Author(s): Benedikt Kuehne
 
-# Description: 
+# Description: Helper/preinstaller module for (unix)-OS other than kali
+# 
 
 I02_custom_os() {
   module_title "${FUNCNAME[0]}"
   echo put your stuff here
+  # mongodb / cve-search
+  # libssl1.1 missing
+  echo "deb http://security.ubuntu.com/ubuntu impish-security main" | tee /etc/apt/sources.list.d/impish-security.list
+  apt-get update
+  apt-get install libssl1.1
+  # other requirements.system
 }
+
+
