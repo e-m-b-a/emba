3f0c865ee7201f5461eea0c073d63d108a0f760f
m-1-k-3 <m1k3@s3cur1ty.de>
2022-07-24 13:37:46 +0200

    fix

--- installer/ID1_ubuntu_os.sh
+++ installer/ID1_ubuntu_os.sh
@@ -37,14 +37,14 @@ ID1_ubuntu_os() {
       # docker installation on Ubuntu jammy in WSL environment is somehow broken
       echo -e "\\n""$MAGENTA""$BOLD""Docker installation for Ubuntu:jammy!""$NC"
 
-      sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common
+      sudo apt-get install lsb-release ca-certificates apt-transport-https software-properties-common -y
 
       curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
       echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-      sudo apt update
-      sudo apt install docker-ce -Y
+      sudo apt-get update
+      sudo apt-get install docker-ce -y
 
       # need to start dockerd manually in WSL environments:
       sudo dockerd --iptables=false &
