c23bdfcddf4594befd2eb7da7f5546f30d840a67
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-07-27 11:21:11 +0200

    ch recommendations

--- emba.sh
+++ emba.sh
@@ -908,7 +908,10 @@ main()
     pkill -f "inotifywait.*$LOG_DIR.*"
   fi
   if [[ -f "$HTML_PATH"/index.html ]] && [[ "$IN_DOCKER" -eq 0 ]]; then
-    print_output "[*] Web report created HTML report in $ORANGE$LOG_DIR/html-report$NC\\n" "main" 
+    print_output "[*] Web report created HTML report in $ORANGE$LOG_DIR/html-report$NC\\n" "main"
+    if grep -q "PRETTY_NAME=\"Ubuntu" /etc/os-release 2>/dev/null && [[ "$LOG_DIR" = /home/* ]]; then
+      print_output "[!] Use $BLUE changeown <USER> $ORANGE$LOG_DIR $BLUE -R $NC\\n" "main"
+    fi
     print_output "[*] Open the web-report with$ORANGE firefox $(abs_path "$HTML_PATH/index.html")$NC\\n" "main"
   fi
 }
