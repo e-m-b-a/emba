4d22ced28630bd9d63c0181f2e3dc4971ecd8cf7
m-1-k-3 <m1k3@s3cur1ty.de>
2022-08-05 17:50:05 +0200

    notification system on ubuntu

--- installer/ID1_ubuntu_os.sh
+++ installer/ID1_ubuntu_os.sh
@@ -26,9 +26,9 @@ ID1_ubuntu_os() {
     apt-get install "${INSTALL_APP_LIST[@]}" -y
 
     if ! [[ -f "/usr/share/dbus-1/services/org.freedesktop.Notifications.service" ]] && [[ -f "/usr/lib/notification-daemon/notification-daemon" ]]; then
-      echo "[D-BUS Service]" > /usr/lib/notification-daemon/notification-daemon
-      echo "Name=org.freedesktop.Notifications" >> /usr/lib/notification-daemon/notification-daemon
-      echo "Exec=/usr/lib/notification-daemon/notification-daemon" >> /usr/lib/notification-daemon/notification-daemon
+      echo "[D-BUS Service]" > /usr/share/dbus-1/services/org.freedesktop.Notifications.service
+      echo "Name=org.freedesktop.Notifications" >> /usr/share/dbus-1/services/org.freedesktop.Notifications.service
+      echo "Exec=/usr/lib/notification-daemon/notification-daemon" >> /usr/share/dbus-1/services/org.freedesktop.Notifications.service
     fi
 
     if ! dpkg -l libssl1.1 &>/dev/null; then
