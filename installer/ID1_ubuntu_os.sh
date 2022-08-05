b08746d38e44de3cd290b5b897519eb22d6a079b
m-1-k-3 <m1k3@s3cur1ty.de>
2022-08-05 17:41:44 +0200

    notification system on ubuntu

--- installer/ID1_ubuntu_os.sh
+++ installer/ID1_ubuntu_os.sh
@@ -21,6 +21,16 @@ ID1_ubuntu_os() {
   if [[ "$OTHER_OS" -eq 1 ]] && [[ "$UBUNTU_OS" -eq 1 ]]; then
     # mongodb / cve-search
     echo -e "\\n""$MAGENTA""$BOLD""Installations for Ubuntu:jammy!""$NC"
+
+    print_tool_info "notification-daemon" 1
+    apt-get install "${INSTALL_APP_LIST[@]}" -y
+
+    if ! [[ -f "/usr/share/dbus-1/services/org.freedesktop.Notifications.service" ]] && [[ -f "/usr/lib/notification-daemon/notification-daemon" ]]; then
+      echo "[D-BUS Service]" > /usr/lib/notification-daemon/notification-daemon
+      echo "Name=org.freedesktop.Notifications" >> /usr/lib/notification-daemon/notification-daemon
+      echo "Exec=/usr/lib/notification-daemon/notification-daemon" >> /usr/lib/notification-daemon/notification-daemon
+    fi
+
     if ! dpkg -l libssl1.1 &>/dev/null; then
         # libssl1.1 missing
         echo -e "\\n""$BOLD""Installing libssl1.1 for mongodb!""$NC"
