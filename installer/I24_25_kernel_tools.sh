f2761bceef632aac5a3e6648e3083999d9e04738
m-1-k-3 <m1k3@s3cur1ty.de>
2022-08-20 12:15:03 +0200

    vmlinux-to-elf

--- installer/I24_25_kernel_tools.sh
+++ installer/I24_25_kernel_tools.sh
@@ -10,10 +10,9 @@
 #
 # EMBA is licensed under GPLv3
 #
-# Author(s): Michael Messner, Pascal Eckmann
-# Contributor(s): Stefan Haboeck, Nikolas Papaioannou
+# Author(s): Michael Messner
 
-# Description:  Installs different open source tools from github
+# Description:  Installs vmlinux-to-elf
 
 I24_25_kernel_tools() {
   module_title "${FUNCNAME[0]}"
@@ -34,7 +33,7 @@ I24_25_kernel_tools() {
     case ${ANSWER:0:1} in
       y|Y )
         apt-get install "${INSTALL_APP_LIST[@]}" -y
-        git clone git@github.com:marin-m/vmlinux-to-elf.git external/vmlinux-to-elf
+        git clone https://github.com/marin-m/vmlinux-to-elf external/vmlinux-to-elf
 
         cd external/vmlinux-to-elf || ( echo "Could not install EMBA component vmlinux-to-elf" && exit 1 )
         pip3 install --upgrade lz4 zstandard git+https://github.com/clubby789/python-lzo@b4e39df
