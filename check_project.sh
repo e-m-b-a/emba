91a041c142b10999f99c3aa1a1b754ec894ac331
m-1-k-3 <m1k3@s3cur1ty.de>
2022-09-25 12:30:52 +0200

    move strict mode settings to function

--- check_project.sh
+++ check_project.sh
@@ -20,14 +20,9 @@ STRICT_MODE=1
 if [[ "$STRICT_MODE" -eq 1 ]]; then
   # shellcheck disable=SC1091
   source ./installer/wickStrictModeFail.sh
-  # http://redsymbol.net/articles/unofficial-bash-strict-mode/
-  # https://github.com/tests-always-included/wick/blob/master/doc/bash-strict-mode.md
-  set -e          # Exit immediately if a command exits with a non-zero status
-  set -u          # Exit and trigger the ERR trap when accessing an unset variable
-  set -o pipefail # The return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status
-  set -E          # The ERR trap is inherited by shell functions, command substitutions and commands in subshells
-  shopt -s extdebug # Enable extended debugging
-  IFS=$'\n\t'     # Set the "internal field separator"
+  # shellcheck disable=SC1091
+  source ./helpers/helpers_emba_load_strict_settings.sh
+  load_strict_mode_settings
   trap 'wickStrictModeFail $?' ERR  # The ERR trap is triggered when a script catches an error
 fi
 
