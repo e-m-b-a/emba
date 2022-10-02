f6a64fa15ffcec89fca0b3bb1588e59c4e2c6386
m-1-k-3 <m1k3@s3cur1ty.de>
2022-10-02 10:54:29 +0200

    nosemgrep

--- helpers/helpers_emba_load_strict_settings.sh
+++ helpers/helpers_emba_load_strict_settings.sh
@@ -22,6 +22,7 @@ load_strict_mode_settings(){
   set -o pipefail # The return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status
   set -E          # The ERR trap is inherited by shell functions, command substitutions and commands in subshells
   shopt -s extdebug # Enable extended debugging
+  # nosemgrep
   IFS=$'\n\t'     # Set the "internal field separator"
   trap 'wickStrictModeFail $?' ERR  # The ERR trap is triggered when a script catches an error
 }
