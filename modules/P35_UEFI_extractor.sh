b2b3e4bf155fd41bf26e7f251148be39af87277c
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-11-14 14:17:18 +0100

    update P35

--- modules/P35_UEFI_extractor.sh
+++ modules/P35_UEFI_extractor.sh
@@ -100,6 +100,9 @@ uefi_extractor(){
   fi
 
   FIRMWARE_NAME_="$(basename "$FIRMWARE_PATH_")"
+  if [[ -d "$EXTRACTION_DIR_" ]]; then
+    mkdir -p "$EXTRACTION_DIR_"
+  fi
   cp "$FIRMWARE_PATH_" "$EXTRACTION_DIR_"
   $UEFI_EXTRACT_BIN "$EXTRACTION_DIR_"/firmware all &> "$LOG_PATH_MODULE"/uefi_extractor_"$FIRMWARE_NAME_".log
   UEFI_EXTRACT_REPORT_FILE="$EXTRACTION_DIR_"/firmware.report.txt
