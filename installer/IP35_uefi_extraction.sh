14fe7fd89499dd7dbbf743a75c7812ffe9f50f30
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-11-19 07:49:17 +0100

    update ip35

--- installer/IP35_uefi_extraction.sh
+++ installer/IP35_uefi_extraction.sh
@@ -24,7 +24,7 @@ IP35_uefi_extraction() {
   print_file_info "UEFIExtract_NE_A62_linux_x86_64.zip" "Release-version A62" "https://github.com/LongSoft/UEFITool/releases/download/A62/UEFIExtract_NE_A62_linux_x86_64.zip" "external/UEFITool/UEFIExtract_NE_A62_linux_x86_64.zip"
   print_tool_info "unzip" 1
 
-  if [[ "$LIST_DEP" -eq 1 ]] || [[ $DOCKER_SETUP -eq 0 ]]; then
+  if [[ "$LIST_DEP" -eq 1 ]] || [[ $DOCKER_SETUP -eq 1 ]]; then
       ANSWER=("n")
   else
       echo -e "\\n""$MAGENTA""$BOLD""UEFI Extraction Tool"" will be downloaded (if not already on the system) installed!""$NC"
@@ -32,10 +32,10 @@ IP35_uefi_extraction() {
   fi
 
   case ${ANSWER:0:1} in
-      y|Y )
+    y|Y )
       apt-get install "${INSTALL_APP_LIST[@]}" -y --no-install-recommends
       if ! [[ -d external/UEFITool ]]; then
-          mkdir external/UEFITool
+        mkdir external/UEFITool
       fi
       download_file "UEFIExtract_NE_A62_linux_x86_64.zip" "https://github.com/LongSoft/UEFITool/releases/download/A62/UEFIExtract_NE_A62_linux_x86_64.zip" "external/UEFITool/UEFIExtract_NE_A62_linux_x86_64.zip"
       if [[ -f "external/UEFITool/UEFIExtract_NE_A62_linux_x86_64.zip" ]]; then
@@ -43,7 +43,7 @@ IP35_uefi_extraction() {
           unzip external/UEFITool/UEFIExtract_NE_A62_linux_x86_64.zip -d external/UEFITool
         fi
       else
-          echo -e "$ORANGE""UEFITool installation failed - check it manually""$NC"
+        echo -e "$ORANGE""UEFITool installation failed - check it manually""$NC"
       fi
       ;;
   esac
