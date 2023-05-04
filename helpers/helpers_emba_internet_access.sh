1ba920521befb480a94bcb6e4edc5633b8db46e8
Benedikt Kuehne <bennimail.4spam@gmail.com>
2023-05-04 10:23:30 +0200

    update ask_chatgpt

--- helpers/helpers_emba_internet_access.sh
+++ helpers/helpers_emba_internet_access.sh
@@ -147,10 +147,10 @@ ask_chatgpt(){
   else
     # test connection
     print_output "[*] Testing API-Key"
-    printf '%s' "Testing API key : $OPENAI_API_KEY " >> "$LOG_DIR"/chatgpt.log # TODO remove
+    printf '%s \n' "Testing API key : $OPENAI_API_KEY " >> "$LOG_DIR"/chatgpt.log # TODO remove
     if ! curl https://api.openai.com/v1/chat/completions -H "Content-Type: application/json" \
             -H "Authorization: Bearer $OPENAI_API_KEY" \
-            -d @"$CONFIG_DIR/gpt_template.json" &>/dev/null ; then
+            -d @"$CONFIG_DIR/gpt_template.json" &>"$LOG_DIR/chatgpt.log" ; then
       print_output "[!] ChatGPT error while testing the API-Key"
       printf '%s' "requests aren't working, aborting" >> "$LOG_DIR"/chatgpt.log # TODO remove
       CHATGPT_RESULT_CNT=-1
