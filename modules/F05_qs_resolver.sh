255f8da3d0cba056c02d6944e547c40eff81a9be
Benedikt Kuehne <bennimail.4spam@gmail.com>
2023-06-26 16:54:16 +0200

    update

--- modules/F05_qs_resolver.sh
+++ modules/F05_qs_resolver.sh
@@ -17,43 +17,44 @@
 F05_qs_resolver(){
   module_log_init "${FUNCNAME[0]}"
   module_title "QS-Resolver"
+  if [[ ${GPT_OPTION} -gt 0 ]]; then
+    # wait for Q02 to end
+    while ! grep -q "Q02_openai_question finished" "${LOG_DIR}"/"${MAIN_LOG_FILE}"; do
+      sleep 1
+    done
+    local SCRIPT_PATH_TMP_=""
+    local GPT_ANCHOR_=""
+    local _GPT_PRIO_=3
+    local GPT_QUESTION_=""
+    local GPT_RESPONSE_=""
+    local GPT_TOKENS_=0
+    local GPT_OUTPUT_FILE_=""
+    local _GPT_INPUT_FILE_=""
 
-  # wait for Q02 to end
-  while ! grep -q "Q02_openai_question finished" "${LOG_DIR}"/"${MAIN_LOG_FILE}"; do
-    sleep 1
-  done
-  local SCRIPT_PATH_TMP_=""
-  local GPT_ANCHOR_=""
-  local _GPT_PRIO_=3
-  local GPT_QUESTION_=""
-  local GPT_RESPONSE_=""
-  local GPT_TOKENS_=0
-  local GPT_OUTPUT_FILE_=""
-  local _GPT_INPUT_FILE_=""
+    if [[ -f "${CSV_DIR}/q02_openai_question.csv" ]]; then
+      while IFS=";" read -r COL1_ COL2_ COL3_ COL4_ COL5_ COL6_ COL7_; do
+        SCRIPT_PATH_TMP_="${COL1_}"
+        GPT_ANCHOR_="${COL2_}"
+        _GPT_PRIO_="${COL3_//GPT-Prio-/}"
+        GPT_QUESTION_="${COL4_}"
+        GPT_RESPONSE_="${COL5_}"
+        GPT_TOKENS_="${COL6_//cost\=/}"
+        GPT_OUTPUT_FILE_="${COL7_}"
+        _GPT_INPUT_FILE_="$(basename "${SCRIPT_PATH_TMP_}")"
 
-  if [[ -f "${CSV_DIR}/q02_openai_question.csv" ]]; then
-    while IFS=";" read -r COL1_ COL2_ COL3_ COL4_ COL5_ COL6_ COL7_; do
-      SCRIPT_PATH_TMP_="${COL1_}"
-      GPT_ANCHOR_="${COL2_}"
-      _GPT_PRIO_="${COL3_//GPT-Prio-/}"
-      GPT_QUESTION_="${COL4_}"
-      GPT_RESPONSE_="${COL5_}"
-      GPT_TOKENS_="${COL6_//cost\=/}"
-      GPT_OUTPUT_FILE_="${COL7_}"
-      _GPT_INPUT_FILE_="$(basename "${SCRIPT_PATH_TMP_}")"
-
-      if [[ ${GPT_TOKENS_} -ne 0 ]]; then
-        GPT_OUTPUT_FILE_=$(find ~+ -iname "$(basename "${GPT_OUTPUT_FILE_}")" )
-        if ! [ -f "${GPT_OUTPUT_FILE_}" ]; then
-          print_output "[-] Something went wrong with the Output file ${GPT_OUTPUT_FILE_}"
-          if [[ -z ${GPT_OUTPUT_FILE_} ]]; then
-            print_output "    there is no file name for anchor: ${GPT_ANCHOR_}"
+        if [[ ${GPT_TOKENS_} -ne 0 ]]; then
+          GPT_OUTPUT_FILE_=$(find ~+ -iname "$(basename "${GPT_OUTPUT_FILE_}")" )
+          if ! [ -f "${GPT_OUTPUT_FILE_}" ]; then
+            print_output "[-] Something went wrong with the Output file ${GPT_OUTPUT_FILE_}"
+            if [[ -z ${GPT_OUTPUT_FILE_} ]]; then
+              print_output "    there is no file name for anchor: ${GPT_ANCHOR_}"
+            fi
+          else
+            sed -i "s/${GPT_ANCHOR_}/${GPT_QUESTION_}\n${GPT_RESPONSE_}\n/1" "${GPT_OUTPUT_FILE_}"
           fi
-        else
-          sed -i "s/${GPT_ANCHOR_}/${GPT_QUESTION_}\n${GPT_RESPONSE_}\n/1" "${GPT_OUTPUT_FILE_}"
         fi
-      fi
-    done < "${CSV_DIR}/q02_openai_question.csv"
+      done < "${CSV_DIR}/q02_openai_question.csv"
+    fi
   fi
   module_end_log "${FUNCNAME[0]}" 1
 }
