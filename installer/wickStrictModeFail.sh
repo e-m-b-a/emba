de7b5ec894b4166c3f39860a6b2b1f0af26439f2
kali <littleleddragon@gmail.com>
2023-10-17 11:33:35 +0200

    wickStrictModeFail: semgrep and shellcheck braces

--- installer/wickStrictModeFail.sh
+++ installer/wickStrictModeFail.sh
@@ -14,9 +14,9 @@ wickStrictModeFail() (
   set +x
   local argsList argsLeft i nextArg
 
-  echo -e "Error detected - status code $ORANGE$1$NC"
-  echo -e "Command:  $ORANGE$BASH_COMMAND$NC"
-  echo -e "Location:  $ORANGE${BASH_SOURCE[1]:-unknown}$NC, line $ORANGE${BASH_LINENO[0]:-unknown}$NC"
+  echo -e "Error detected - status code ${ORANGE}${1}${NC}"
+  echo -e "Command:  ${ORANGE}${BASH_COMMAND}${NC}"
+  echo -e "Location:  ${ORANGE}${BASH_SOURCE[1]:-unknown}${NC}, line ${ORANGE}${BASH_LINENO[0]:-unknown}${NC}"
 
   if [[ ${#PIPESTATUS[@]} -gt 1 ]]; then
       echo "Pipe status: " "${PIPESTATUS[@]}"
@@ -25,20 +25,20 @@ wickStrictModeFail() (
   i=$#
   nextArg=$#
 
-  if [[ $i -lt ${#BASH_LINENO[@]} ]]; then
+  if [[ ${i} -lt ${#BASH_LINENO[@]} ]]; then
     echo "Stack Trace:"
   else
     echo "Stack trace is unavailable"
   fi
 
-  while [[ $i -lt ${#BASH_LINENO[@]} ]]; do
+  while [[ ${i} -lt ${#BASH_LINENO[@]} ]]; do
     argsList=()
 
-    if [[ ${#BASH_ARGC[@]} -gt $i ]] && [[ ${#BASH_ARGV[@]} -ge $(( nextArg + BASH_ARGC[i] )) ]]; then
+    if [[ ${#BASH_ARGC[@]} -gt ${i} ]] && [[ ${#BASH_ARGV[@]} -ge $(( nextArg + BASH_ARGC[i] )) ]]; then
       for (( argsLeft = BASH_ARGC[i]; argsLeft; --argsLeft )); do
         # Note: this reverses the order on purpose
         # shellcheck disable=SC2004
-        argsList[$argsLeft]=${BASH_ARGV[nextArg]}
+        argsList[${argsLeft}]=${BASH_ARGV[nextArg]}
         (( nextArg ++ ))
       done
 
@@ -55,7 +55,7 @@ wickStrictModeFail() (
       argsList=""
     fi
 
-    echo "    [$i] ${FUNCNAME[i]:+${FUNCNAME[i]}(): }${BASH_SOURCE[i]}, line ${BASH_LINENO[i - 1]} -> ${FUNCNAME[i]:-${BASH_SOURCE[i]##*/}}$argsList"
+    echo "    [${i}] ${FUNCNAME[i]:+${FUNCNAME[i]}"():" }${BASH_SOURCE[i]}, line ${BASH_LINENO[i - 1]} -> ${FUNCNAME[i]:-${BASH_SOURCE[i]##*/}}${argsList}"
     (( i ++ ))
   done
   echo -e "\n${BLUE}${BOLD}Important: Consider filling out a bug report at https://github.com/e-m-b-a/emba/issues${NC}\n"
