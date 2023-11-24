b742a2a5c54f47c91986c5a768edcda38f373879
m-1-k-3 <m1k3@s3cur1ty.de>
2023-11-24 23:21:22 +0100

    s26

--- modules/S26_kernel_vuln_verifier.sh
+++ modules/S26_kernel_vuln_verifier.sh
@@ -142,7 +142,7 @@ S26_kernel_vuln_verifier()
       continue
     fi
 
-    CVE_DETAILS_PATH="${LOG_PATH_MODULE}""/kernel-${K_VERSION}-vulns.json"
+    local CVE_DETAILS_PATH="${LOG_PATH_MODULE}""/kernel-${K_VERSION}-vulns.json"
 
     if [[ -f "${KERNEL_ELF_PATH}" ]]; then
       extract_kernel_arch "${KERNEL_ELF_PATH}"
@@ -189,7 +189,7 @@ S26_kernel_vuln_verifier()
       tar -xzf "${KERNEL_ARCH_PATH}/linux-${K_VERSION_KORG}.tar.gz" -C "${LOG_PATH_MODULE}"
     fi
 
-    get_cve_kernel_data "${K_VERSION}"
+    get_cve_kernel_data "${K_VERSION}" "${CVE_DETAILS_PATH}"
 
     if ! [[ -f "${CVE_DETAILS_PATH}" ]]; then
       print_output "[-] No CVE details generated ... check for further kernel version"
@@ -335,6 +335,8 @@ split_symbols_file() {
 get_cve_kernel_data() {
   sub_module_title "Version based vulnerability detection"
   local K_VERSION_="${1:-}"
+  local CVE_K_PATH="${2:-}"
+
   local CVE_VER_SOURCES_ARR_KERNEL=()
   local CVE_VER_SOURCES_FILE=""
   local CVE_ID=""
@@ -342,7 +344,7 @@ get_cve_kernel_data() {
   local CVE_V31=""
 
   print_output "[*] Extract CVE data for kernel version ${ORANGE}${K_VERSION_}${NC}"
-  # "linux_kernel:""${K_VERSION_}"":" -o json > "${CVE_DETAILS_PATH}"
+  # "linux_kernel:""${K_VERSION_}"":" -o json > "${CVE_K_PATH}"
   mapfile -t CVE_VER_SOURCES_ARR_KERNEL < <(grep -l -r "cpe.*linux_kernel:${K_VERSION_}:" "${NVD_DIR}" || true)
   for CVE_VER_SOURCES_FILE in "${CVE_VER_SOURCES_ARR_KERNEL[@]}"; do
     CVE_ID=$(jq -r '.id' "${CVE_VER_SOURCES_FILE}")
@@ -352,10 +354,10 @@ get_cve_kernel_data() {
     if [[ -f "${LOG_PATH_MODULE}"/"${VERSION_PATH}".txt ]]; then
       # check if we have already an entry for this CVE - if not, we will write it to the output file
       if ! grep -q "^${CVE_ID}:" "${LOG_PATH_MODULE}"/kernel_"${K_VERSION_}"-vulns.txt; then
-        echo "${CVE_ID}:${CVE_V2:-"NA"}:${CVE_V31:-"NA"}:${CVE_SUMMARY}" >> "${CVE_DETAILS_PATH}" || true
+        echo "${CVE_ID}:${CVE_V2:-"NA"}:${CVE_V31:-"NA"}:${CVE_SUMMARY}" >> "${CVE_K_PATH}" || true
       fi
     else
-      echo "${CVE_ID}:${CVE_V2:-"NA"}:${CVE_V31:-"NA"}:${CVE_SUMMARY}" >> "${CVE_DETAILS_PATH}" || true
+      echo "${CVE_ID}:${CVE_V2:-"NA"}:${CVE_V31:-"NA"}:${CVE_SUMMARY}" >> "${CVE_K_PATH}" || true
     fi
   done
 }
