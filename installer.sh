a558f81a606dede7ca71a2296cf72cdc4beb8eaa
m-1-k-3 <m1k3@s3cur1ty.de>
2023-12-13 11:55:21 +0100

    docker v7

--- installer.sh
+++ installer.sh
@@ -296,11 +296,13 @@ if [[ $(version "${DOCKER_COMP_VER}") -lt $(version "1.28.5") ]]; then
 fi
 
 # check pip3 docker version TODO move into pipenv
+export DOCKER_COMPOSE="docker-compose"
 DOCKER_VER=$(pip3 show docker | grep Version: | awk '{print $2}' || true)
 if [[ $(version "${DOCKER_VER}") -ge $(version "7.0.0") ]]; then
   echo -e "\n${ORANGE}WARNING: compatibility of the used docker version is unknown!${NC}"
   echo -e "\n${ORANGE}Please consider downgrading your pip3 docker version. \$pip3 install \"docker<7.0.0\"${NC}"
   read -p "If you know what you are doing you can press any key to continue ..." -n1 -s -r
+  export DOCKER_COMPOSE="docker compose"
 fi
 
 if [[ "${OTHER_OS}" -eq 1 ]]; then
