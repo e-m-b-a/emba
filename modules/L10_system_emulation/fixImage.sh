ec068c75125040bc95daa40683a7774579f4b3e2
m-1-k-3 <m1k3@s3cur1ty.de>
2024-01-02 16:22:58 +0100

    update copyright

--- modules/L10_system_emulation/fixImage.sh
+++ modules/L10_system_emulation/fixImage.sh
@@ -1,6 +1,6 @@
 # Copyright (c) 2015 - 2016, Daming Dominic Chen
 # Copyright (c) 2017 - 2020, Mingeun Kim, Dongkwan Kim, Eunsoo Kim
-# Copyright (c) 2022 - 2023 Siemens Energy AG
+# Copyright (c) 2022 - 2024 Siemens Energy AG
 
 # use busybox statically-compiled version of all binaries
 # shellcheck disable=SC2148
