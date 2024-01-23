e5449c1648e8e72e4c31b792afe891e4b0e63df1
kali <littleleddragon@gmail.com>
2024-01-23 12:20:27 +0100

    identifying BMC AST2400

--- modules/S06_distribution_identification.sh
+++ modules/S06_distribution_identification.sh
@@ -22,6 +22,7 @@ S06_distribution_identification()
   pre_module_reporter "${FUNCNAME[0]}"
 
   export DLINK_FW_VER=""
+  export BMC_FW_VER=""
   local OUTPUT=0
   local FILE_QUOTED
   local PATTERN=""
@@ -77,8 +78,20 @@ S06_distribution_identification()
     fi
   done < "${CONFIG_DIR}"/distri_id.cfg
 
+  bmc_identifier
+
   write_log ""
   module_end_log "${FUNCNAME[0]}" "${OUTPUT}"
+}
+
+bmc_identifier(){
+  local BMC_CONFIG="${FIRMWARE_PATH}/sbin/net-snmp-config"
+  
+  if [[ -f "${BMC_CONFIG}" && "${ARCH}" == "ARM" ]]; then
+    if grep -q "AST2400" "${BMC_CONFIG}" && grep -q "PLATFORM_SUPERMICRO" "${BMC_CONFIG}" ; then
+        BMC_FW_VER="supermicrox11-bmc"
+    fi
+  fi
 }
 
 dlink_image_sign() {
