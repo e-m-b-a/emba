8817aaee546396b2fa886d0596a8a08c52c42b48
m-1-k-3 <m1k3@s3cur1ty.de>
2024-02-18 12:31:14 +0100

    prepare 1.4.0-pre

--- helpers/create_version.sh
+++ helpers/create_version.sh
@@ -16,7 +16,8 @@
 
 create_version(){
   local VERSION=""
-  VERSION="$(echo "$(grep "export EMBA_VERSION=" helpers/helpers_emba_defaults.sh | cut -d\" -f2)"-"$(git describe --always)")"
+  # VERSION="$(echo "$(grep "export EMBA_VERSION=" helpers/helpers_emba_defaults.sh | cut -d\" -f2)"-"$(git describe --always)")"
+  VERSION="$(echo "$(grep "export EMBA_VERSION=" helpers/helpers_emba_defaults.sh | cut -d\" -f2)")"
   echo "${VERSION}" > config/VERSION.txt
 }
 
