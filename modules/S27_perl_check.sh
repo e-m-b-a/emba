dcb3fd049a80012eb50d96ea25a3e7e72d471d89
m-1-k-3 <m1k3@s3cur1ty.de>
2024-02-22 15:11:33 +0100

    make it work - s27

--- modules/S27_perl_check.sh
+++ modules/S27_perl_check.sh
@@ -31,18 +31,29 @@ S27_perl_check()
   local lS27_PL_SCRIPTS=0
   local lPL_SCRIPT=""
   local lPERL_SCRIPTS_ARR=()
+  local lNAME=""
   local lWAIT_PIDS_S27=()
+
+  export PERL5LIB="${EXT_DIR}"/zarn/lib
 
   write_csv_log "Script path" "Perl issues detected" "common linux file" "vuln title" "vuln line nr" "vuln note"
   mapfile -t lPERL_SCRIPTS_ARR < <(find "${FIRMWARE_PATH}" -xdev -type f \( -name "*.pl" -o -name "*.pm" -o -name "*.cgi" \) -exec md5sum {} \; 2>/dev/null | sort -u -k1,1 | cut -d\  -f3 )
   for lPL_SCRIPT in "${lPERL_SCRIPTS_ARR[@]}" ; do
     if ( file "${lPL_SCRIPT}" | grep -q "Perl script.*executable" ) ; then
+      if [[ -f "${BASE_LINUX_FILES}" && "${FULL_TEST}" -eq 0 ]]; then
+        # if we have the base linux config file we only test non known Linux binaries
+        # with this we do not waste too much time on open source Linux stuff
+        lNAME=$(basename "${lPL_SCRIPT}" 2> /dev/null)
+        if grep -E -q "^${lNAME}$" "${BASE_LINUX_FILES}" 2>/dev/null; then
+          continue
+        fi
+      fi
       ((lS27_PL_SCRIPTS+=1))
       if [[ "${THREADED}" -eq 1 ]]; then
         s27_zarn_perl_checks "${lPL_SCRIPT}" &
         local lTMP_PID="$!"
         store_kill_pids "${lTMP_PID}"
-        WAIT_PIDS_S27+=( "${lTMP_PID}" )
+        lWAIT_PIDS_S27+=( "${lTMP_PID}" )
         max_pids_protection "${MAX_MOD_THREADS}" "${lWAIT_PIDS_S27[@]}"
         continue
       else
@@ -52,7 +63,11 @@ S27_perl_check()
   done
 
   [[ "${THREADED}" -eq 1 ]] && wait_for_pid "${lWAIT_PIDS_S27[@]}"
-  [[ -d "${TMP_DIR}"/s27 ]] && rm -r "${TMP_DIR}"/s27
+  [[ -d "${TMP_DIR}"/s27 ]] && ( rm -r "${TMP_DIR}"/s27 || true )
+
+  if [[ -f "${TMP_DIR}"/S27_VULNS.tmp ]]; then
+    lS27_PL_VULNS=$(awk '{sum += $1 } END { print sum }' "${TMP_DIR}"/S27_VULNS.tmp)
+  fi
 
   write_log ""
   write_log "[*] Statistics:${lS27_PL_VULNS}:${lS27_PL_SCRIPTS}"
@@ -62,7 +77,7 @@ S27_perl_check()
 s27_zarn_perl_checks() {
   local lPL_SCRIPT="${1:-}"
   local lNAME=""
-  local lPL_LOG=""
+  local lPL_LOG=""  # text log with original output from zarn analysis
   local lVULNS=""
   local lGPT_PRIO=2
   local lGPT_ANCHOR=""
@@ -71,8 +86,11 @@ s27_zarn_perl_checks() {
   local lZARN_VULN_TITLE=""
   local lZARN_VULN_MESSAGE=""
   local lZARN_VULN_LINE=""
+  local lCODE_LINE=""
+  local lPL_LOG_LINKED=""  # 2nd stage log which is linked from the main log
 
   lNAME=$(basename "${lPL_SCRIPT}" 2> /dev/null | sed -e 's/:/_/g')
+  print_output "[*] Testing perl script ${ORANGE}${lPL_SCRIPT}${NC}" "no_log"
   lPL_LOG="${LOG_PATH_MODULE}""/zarn_""${lNAME}"".txt"
   if [[ "${lPL_SCRIPT: -4}" == ".cgi" ]]; then
     if ! [[ -d "${TMP_DIR}"/s27 ]]; then
@@ -80,12 +98,15 @@ s27_zarn_perl_checks() {
     fi
     # as zarn does not like cgi's, we need to temp rename these files now
     cp "${lPL_SCRIPT}" "${TMP_DIR}"/s27/"${lNAME/.cgi/.pl}"
-    perl "${EXT_DIR}"/zarn/zarn.pl -r "${EXT_DIR}"/zarn/rules/default.yml --source "${TMP_DIR}"/s27/"${lNAME/.cgi/.pl}" --sarif "${LOG_PATH_MODULE}""/zarn_""${lNAME}"".sarif" > "${lPL_LOG}" 2> /dev/null || true
-    rm "${TMP_DIR}"/s27/"${lNAME/.cgi/.pl}"
+    if [[ -f "${TMP_DIR}"/s27/"${lNAME/.cgi/.pl}" ]]; then
+      perl "${EXT_DIR}"/zarn/zarn.pl -r "${EXT_DIR}"/zarn/rules/default.yml --source "${TMP_DIR}"/s27/"${lNAME/.cgi/.pl}" --sarif "${LOG_PATH_MODULE}""/zarn_""${lNAME}"".sarif" | tee -a "${lPL_LOG}" || print_output "[-] Analysis of ${lNAME} failed" "no_log"
+      rm "${TMP_DIR}"/s27/"${lNAME/.cgi/.pl}" || true
+    fi
   else
-    perl "${EXT_DIR}"/zarn/zarn.pl -r "${EXT_DIR}"/zarn/rules/default.yml --source "${lPL_SCRIPT}" --sarif "${LOG_PATH_MODULE}""/zarn_""${lNAME}"".sarif" > "${lPL_LOG}" 2> /dev/null || true
+    perl "${EXT_DIR}"/zarn/zarn.pl -r "${EXT_DIR}"/zarn/rules/default.yml --source "${lPL_SCRIPT}" --sarif "${LOG_PATH_MODULE}""/zarn_""${lNAME}"".sarif" | tee -a "${lPL_LOG}" || print_output "[-] Analysis of ${lNAME} failed" "no_log"
   fi
 
+  ## reporting starts here
   lVULNS=$(grep -c "\[vuln\]" "${lPL_LOG}" 2> /dev/null || true)
   if [[ "${lVULNS}" -gt 0 ]] ; then
     # check if this is common linux file:
@@ -102,21 +123,42 @@ s27_zarn_perl_checks() {
       lCOMMON_FILES_FOUND=""
       lCFF="NA"
     fi
+    lPL_LOG_LINKED="${LOG_PATH_MODULE}""/zarn_""${lNAME}""_details.txt"
+    print_output "[+] Found ""${RED}""${lVULNS}"" possible issue(s)""${GREEN}"" in perl script ""${lCOMMON_FILES_FOUND}"":""${ORANGE}"" ""$(print_path "${lPL_SCRIPT}")" "" "${lPL_LOG_LINKED}"
     if [[ "${lVULNS}" -gt 20 ]] ; then
-      print_output "[+] Found ""${RED}""${lVULNS}"" issues""${GREEN}"" in script ""${lCOMMON_FILES_FOUND}"":""${NC}"" ""$(print_path "${lPL_SCRIPT}")" ""  "${lPL_LOG}"
-      lGPT_PRIO=3
-    else
-      print_output "[+] Found ""${ORANGE}""${lVULNS}"" issues""${GREEN}"" in script ""${lCOMMON_FILES_FOUND}"":""${NC}"" ""$(print_path "${lPL_SCRIPT}")" "" "${lPL_LOG}"
+      lGPT_PRIO=$((lGPT_PRIO+1))
     fi
 
     mapfile -t lZARN_SARIF_RESULTS_ARR < <(jq -rc '.runs[].results[]' "${LOG_PATH_MODULE}""/zarn_""${lNAME}"".sarif")
+
+    if ! [[ -d "${LOG_PATH_MODULE}"/pl_source_files/ ]]; then
+      mkdir "${LOG_PATH_MODULE}"/pl_source_files/
+    fi
+
+    write_log "[+] Found ""${ORANGE}""${lVULNS}"" possible issue(s)""${GREEN}"" in perl script ""${lCOMMON_FILES_FOUND}"":""${ORANGE}"" ""$(print_path "${lPL_SCRIPT}")" "${lPL_LOG_LINKED}"
+    write_link "${LOG_PATH_MODULE}"/pl_source_files/"${lNAME}".txt "${lPL_LOG_LINKED}"
+    write_log "$(indent "$(orange "$(file "${lPL_SCRIPT}")")")" "${lPL_LOG_LINKED}"
+    write_log "\n" "${lPL_LOG_LINKED}"
+
     for lSARIF_RESULT in "${lZARN_SARIF_RESULTS_ARR[@]}"; do
       lZARN_VULN_TITLE=$(echo "${lSARIF_RESULT}" | jq -rc '.properties.title' || true)
       lZARN_VULN_MESSAGE=$(echo "${lSARIF_RESULT}" | jq -rc '.message.text' || true)
-      lZARN_VULN_LINE=$(echo "${lSARIF_RESULT}" | jq -rc 'locations[].physicalLocation.region.startLine' || true)
+      lZARN_VULN_LINE=$(echo "${lSARIF_RESULT}" | jq -rc '.locations[].physicalLocation.region.startLine' || true)
 
       write_csv_log "$(print_path "${lPL_SCRIPT}")" "${lVULNS}" "${lCFF}" "${lZARN_VULN_TITLE}" "${lZARN_VULN_LINE}" "${lZARN_VULN_MESSAGE}"
-      print_output "$(indent "$(green "${lNAME} - ${lZARN_VULN_TITLE} - line ${lZARN_VULN_LINE}")")"
+      write_log "$(indent "$(indent "Vulnerability title: ${ORANGE}${lZARN_VULN_TITLE}${NC}")")" "${lPL_LOG_LINKED}"
+      write_log "$(indent "$(indent "Vulnerability description: ${ORANGE}${lZARN_VULN_MESSAGE}${NC}")")" "${lPL_LOG_LINKED}"
+      write_log "" "${lPL_LOG_LINKED}"
+
+      # we need the original perl script to link to it and to highlight the affected lines of code
+      if ! [[ -f "${LOG_PATH_MODULE}"/pl_source_files/"${lNAME}".txt ]]; then
+        cp "${lPL_SCRIPT}" "${LOG_PATH_MODULE}"/pl_source_files/"${lNAME}".txt || print_output "[-] Copy of ${lPL_SCRIPT} to log directory failed" "no_log"
+      fi
+
+      lCODE_LINE="$(strip_color_codes "$(sed -n "${lZARN_VULN_LINE}"p "${LOG_PATH_MODULE}/pl_source_files/${lNAME}.txt" 2>/dev/null)")"
+      sed -i -r "${lZARN_VULN_LINE}s/.*/\x1b[32m&\x1b[0m/" "${LOG_PATH_MODULE}/pl_source_files/${lNAME}.txt" || true
+      write_log "$(indent "$(indent "${GREEN}${lZARN_VULN_LINE}${NC} - ${ORANGE}${lCODE_LINE}${NC}")")" "${lPL_LOG_LINKED}"
+      write_log "\\n-----------------------------------------------------------------\\n" "${lPL_LOG_LINKED}"
     done
 
     # writing gpt details:
