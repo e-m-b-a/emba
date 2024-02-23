852bfac6f3ffb5c29ea87da98657a98a10836af6
m-1-k-3 <m1k3@s3cur1ty.de>
2024-02-23 11:41:03 +0100

    zarn install dev branch

--- installer/I20_sourcecode_check.sh
+++ installer/I20_sourcecode_check.sh
@@ -62,7 +62,9 @@ I20_sourcecode_check() {
 
         # zarn perl code analyser
         if ! [[ -d external/zarn ]]; then
-          git clone https://github.com/EMBA-support-repos/zarn.git external/zarn
+          # git clone https://github.com/EMBA-support-repos/zarn.git external/zarn
+          # https://github.com/htrgouvea/zarn/issues/3
+          git clone --branch develop https://github.com/htrgouvea/zarn.git external/zarn
           cd external/zarn || ( echo "Could not install EMBA component zarn" && exit 1 )
           cpanm --installdeps .
           cd "${HOME_PATH}" || ( echo "Could not install EMBA component zarn" && exit 1 )
