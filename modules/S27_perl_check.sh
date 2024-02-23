7cc4617860a4e1738b84860f90e73c6522f89fec
m-1-k-3 <m1k3@s3cur1ty.de>
2024-02-23 16:33:58 +0100

    add url

--- modules/S27_perl_check.sh
+++ modules/S27_perl_check.sh
@@ -2,7 +2,7 @@
 
 # EMBA - EMBEDDED LINUX ANALYZER
 #
-# Copyright 2020-2024 Siemens Energy AG
+# Copyright 2024-2024 Siemens Energy AG
 #
 # EMBA comes with ABSOLUTELY NO WARRANTY. This is free software, and you are
 # welcome to redistribute it under the terms of the GNU General Public License.
@@ -12,7 +12,8 @@
 #
 # Author(s): Michael Messner
 
-# Description:  Checks for bugs, stylistic errors, etc. in perl scripts with zarn - https://github.com/htrgouvea/zarn
+# Description:  Checks for bugs and possible vulnerabilities in perl scripts with zarn - https://github.com/htrgouvea/zarn
+#               See also https://heitorgouvea.me/2023/03/19/static-security-analysis-tool-perl
 
 S27_perl_check()
 {
