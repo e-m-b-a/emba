5ad5c6cb4154f0630a8f9cda340fb560cba35061
m-1-k-3 <m1k3@s3cur1ty.de>
2024-02-26 08:27:12 +0100

    fix docker image update check

--- helpers/helpers_emba_dependency_check.sh
+++ helpers/helpers_emba_dependency_check.sh
@@ -163,7 +163,8 @@ check_git_hash(){
 check_docker_image(){
   local REMOTE_DOCKER_HASH="${1:-}"
   local LOCAL_DOCKER_HASH=""
-  LOCAL_DOCKER_HASH="$(docker image inspect embeddedanalyzer/emba:latest --format '{{json .RepoDigests}}' | jq . | grep "sha" | sed -E 's/.*sha256:([0-9|[a-z]+)"/\1/' || true)"
+  LOCAL_DOCKER_HASH="$(docker inspect --format='{{.RepoDigests}}' embeddedanalyzer/emba:latest | cut -d ':' -f2 || true)"
+
 
   if [[ "${LOCAL_DOCKER_HASH}" == "${REMOTE_DOCKER_HASH}" ]]; then
     echo -e "    Docker image version - ${GREEN}ok${NC}"
