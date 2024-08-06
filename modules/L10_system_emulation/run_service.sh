9a219611aec36984e2603c8642acd47fb2f14ad2
m-1-k-3 <m1k3@s3cur1ty.de>
2024-08-06 16:35:01 +0200

    shellcheck

--- modules/L10_system_emulation/run_service.sh
+++ modules/L10_system_emulation/run_service.sh
@@ -10,7 +10,7 @@
 
 BUSYBOX=/firmadyne/busybox
 # we should build a real and useful PATH ... currently it is just guessing
-export PATH=$PATH:/bin/:/sbin/:/usr/bin/:/usr/sbin:/usr/local/bin:/usr/local/sbin
+export PATH="${PATH}":/bin/:/sbin/:/usr/bin/:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
 # "${BUSYBOX}" touch /firmadyne/EMBA_service_init_done
 ORANGE="\033[0;33m"
