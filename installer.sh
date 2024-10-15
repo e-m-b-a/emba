12f936546491f36c08b796d2b7ac4481ffb4de8a
m-1-k-3 <m1k3@s3cur1ty.de>
2024-10-15 12:31:22 +0200

    fix package anem

--- installer.sh
+++ installer.sh
@@ -300,8 +300,8 @@ elif ! command -v docker > /dev/null || ! command -v docker compose > /dev/null 
   chmod a+r /etc/apt/keyrings/docker.asc
   # Add the repository to Apt sources:
   if [[ "${UBUNTU_OS}" -eq 1 ]]; then
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/${OS} \
     # shellcheck source=/dev/null
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/${OS} \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
   else
     # probably a kali linux
@@ -309,7 +309,7 @@ elif ! command -v docker > /dev/null || ! command -v docker compose > /dev/null 
     bookworkm stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
   fi
   apt-get update -y
-  apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker compose-plugin
+  apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   export DOCKER_COMPOSE=("docker" "compose")
 fi
 
