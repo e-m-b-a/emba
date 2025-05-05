2820145c67da048e35456708a94a048c67dd34e7
HoxhaEndri <littleleddragon@gmail.com>
2025-05-05 10:50:18 +0200

    add variable

--- helpers/helpers_emba_defaults.sh
+++ helpers/helpers_emba_defaults.sh
@@ -78,6 +78,7 @@ set_defaults() {
   export QEMU_KILL_SIZE="10M"
   # export L10_KERNEL_V_LONG="4.1.17"
   export L10_KERNEL_V_LONG="4.1.52"
+  # export L10_KERNEL_V_LONG="4.14.336"
   export L10_BB_VER="1.36.1"
   export FULL_TEST=0            # with this variable we can control the behavior of s16 and s120 -> 0 is default an tests only
                                 # non Linux binaries (binaries not listed in config/linux_common_files.txt. 1 means we test every
