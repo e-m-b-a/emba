518f00772d24fa59174c6f05fc4a910183b4dbbf
Thomas Gingele <77125551+B1TC0R3@users.noreply.github.com>
2025-05-27 20:21:48 +0200

    Update modules/S28_python_run/embamodule.py
    Co-authored-by: Benedikt Kuehne <62940240+BenediktMKuehne@users.noreply.github.com>

--- modules/S28_python_run/embamodule.py
+++ modules/S28_python_run/embamodule.py
@@ -5,7 +5,7 @@
 """
 EMBA - EMBEDDED LINUX ANALYZER
 
-Copyright 2024-2024 Thomas Gingele <b1tc0r3@proton.me>
+Copyright 2024-2025 Thomas Gingele <b1tc0r3@proton.me>
 
 EMBA comes with ABSOLUTELY NO WARRANTY. This is free software, and you are
 welcome to redistribute it under the terms of the GNU General Public License.
