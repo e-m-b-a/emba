2c58356a64e11bfad6b8954856cdfdcad1254eef
m-1-k-3 <m1k3@s3cur1ty.de>
2025-06-06 16:22:49 +0200

    update cve-bin-tool db update module

--- installer/IF17_cve_bin_tool.sh
+++ installer/IF17_cve_bin_tool.sh
@@ -48,25 +48,56 @@ IF17_cve_bin_tool() {
 
         # we need a more up to date protobuf for the android extractor -> lets update it now
         pip_install "protobuf==5.*"
-        cd "${HOME_PATH}" || ( echo "Could not install EMBA component cve-bin-tool" && exit 1 )
-        # if a cve-datbase file is available in the config directory
-        # we start our container building with importing this cve-database
-        # if no db is available we skip this step and update it
-        if [[ -f /installer/cve-database.db ]]; then
-          echo "[*] Importing CVE database from /installer/cve-database.db"
-          python3 external/cve-bin-tool/cve_bin_tool/cli.py --import /installer/cve-database.db || true
-        fi
-        echo "[*] Updating CVE database"
-        python3 external/cve-bin-tool/cve_bin_tool/cli.py --update now -n json-mirror || true
-        echo "[*] Exporting CVE database"
-        python3 external/cve-bin-tool/cve_bin_tool/cli.py --export /installer/cve-database.db || true
-        rm -r "${HOME}"/.cache/cve-bin-tool
 
-        if ! [[ -f /installer/cve-database.db ]]; then
-          echo "ERROR: Could not build cve-bin-tool database"
+        echo "[*] Preparing test data ..."
+        # preparing test data to ensure our CVE database is working as expected
+        echo "product,vendor,version" > "./cve_bin_tool_health_check.csv"
+        echo "busybox,NOTDEFINED,1.14.1" >> "./cve_bin_tool_health_check.csv"
+
+        if ! [[ -d "${HOME}"/.cache/cve-bin-tool ]]; then
+          mkdir "${HOME}"/.cache/cve-bin-tool
+        fi
+
+        if [[ -f /installer/cve-database.db ]]; then
+          echo "[*] Testing the CVE database with the following test data:"
+          cat ./cve_bin_tool_health_check.csv
+
+          echo "[*] Importing CVE database from /installer/cve-database.db"
+          python3 ./cve_bin_tool/cli.py --import /installer/cve-database.db || true
+          # testing import
+          python3 ./cve_bin_tool/cli.py -i ./cve_bin_tool_health_check.csv --disable-version-check --disable-validation-check --no-0-cve-report --offline -f csv -o /tmp/cve_bin_tool_health_check_results || true
+
+          echo "[*] CVE query results for busybox query after initial import:"
+          cat "/tmp/cve_bin_tool_health_check_results.csv"
+          rm "/tmp/cve_bin_tool_health_check_results.csv"
+        else
+          echo "[-] WARNING: No CVE database update file available from installer directory /installer/cve-database.db"
+        fi
+
+        echo "[*] Updating CVE database"
+        python3 ./cve_bin_tool/cli.py --update now -n json-mirror || true
+        # testing db update
+        python3 ./cve_bin_tool/cli.py -i ./cve_bin_tool_health_check.csv --disable-version-check --disable-validation-check --no-0-cve-report --offline -f csv -o /tmp/cve_bin_tool_health_check_results || true
+
+        echo "[*] CVE query results for busybox query after database update:"
+        cat /tmp/cve_bin_tool_health_check_results.csv
+        rm /tmp/cve_bin_tool_health_check_results.csv
+
+        cd "${HOME_PATH}" || ( echo "Could not install EMBA component cve-bin-tool" && exit 1 )
+        echo "[*] Exporting CVE database"
+
+        python3 external/cve-bin-tool/cve_bin_tool/cli.py --export external/cve-bin-tool/cve-database.db || true
+
+        # cleanup
+        rm -r "${HOME}"/.cache/cve-bin-tool
+        rm external/cve-bin-tool/cve_bin_tool_health_check.csv
+
+        if [[ -f external/cve-bin-tool/cve-database.db ]]; then
+          echo -e "${GREEN}[+] CVE database installed${NC}"
+        else
+          echo -e "${ORANGE}ERROR: Could not build cve-bin-tool database${NC}"
           exit 1
         fi
-        # cp -pr "${HOME}"/.cache/cve-bin-tool ./external/cve-bin-tool/cache_cve-bin-tool
       ;;
     esac
   fi
