18ced9217a9d9b5e4366b1e38114eb69e94783a6
m-1-k-3 <m1k3@s3cur1ty.de>
2025-08-08 07:51:19 +0200

    cleanup

--- modules/S23_lua_check.sh
+++ modules/S23_lua_check.sh
@@ -28,32 +28,30 @@ S23_lua_check()
   local lWAIT_PIDS_S23_ARR=()
 
   write_csv_log "Script path" "LUA issues detected" "LUA vulnerabilities detected" "common linux file"
-  # mapfile -t lS23_LUA_SCRIPTS_ARR < <(find "${FIRMWARE_PATH}" -xdev -type f -iname "*.lua" -print0|xargs -r -0 -P 16 -I % sh -c 'md5sum "%" 2>/dev/null || true' | sort -u -k1,1 | cut -d\  -f3 || true)
+  # The s23_luaseccheck is better in identification of the lua scripts
+  # Todo: make the lua identification available for the complete module
   mapfile -t lS23_LUA_SCRIPTS_ARR < <(grep ".lua;" "${P99_CSV_LOG}" | sort -u || true)
 
   sub_module_title "LUA linter checks module"
 
   for lLUA_SCRIPT in "${lS23_LUA_SCRIPTS_ARR[@]}" ; do
-    if [[ "${THREADED}" -eq 1 ]]; then
-      # linting check:
-      s23_luacheck "$(echo "${lLUA_SCRIPT}" | cut -d';' -f2)" &
-      local lTMP_PID="$!"
-      store_kill_pids "${lTMP_PID}"
-      lWAIT_PIDS_S23_ARR+=( "${lTMP_PID}" )
-      max_pids_protection "${MAX_MOD_THREADS}" lWAIT_PIDS_S23_ARR
-      continue
-    else
-      s23_luacheck "$(echo "${lLUA_SCRIPT}" | cut -d';' -f2)"
-    fi
+    # linting check:
+    s23_luacheck "$(echo "${lLUA_SCRIPT}" | cut -d';' -f2)" &
+    local lTMP_PID="$!"
+    lWAIT_PIDS_S23_ARR+=( "${lTMP_PID}" )
+    max_pids_protection "${MAX_MOD_THREADS}" lWAIT_PIDS_S23_ARR
+    continue
   done
 
-  [[ "${THREADED}" -eq 1 ]] && wait_for_pid "${lWAIT_PIDS_S23_ARR[@]}"
+  wait_for_pid "${lWAIT_PIDS_S23_ARR[@]}"
 
   # simple lua checks to identify files which should be analysed in more detail
+  # we should thread this in the future
   print_ln
   s23_luaseccheck
 
   lS23_LUA_ISSUES=$(wc -l 2>/dev/null < "${S23_CSV_LOG}")
+  # extract the not 0 results of the vulnerabilities
   lS23_LUA_VULNS=$(cut -d ';' -f 3 "${S23_CSV_LOG}" | grep -v -c "^0$")
 
   # first line is the header
