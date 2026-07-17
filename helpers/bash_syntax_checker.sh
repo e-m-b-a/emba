79bacdfe53c0b81b725aad4d641b437072c52072
Michael Messner <m1k3@s3cur1ty.de>
2026-07-17 17:08:31 +0200

    bash syntax checker workflow

---
+++ helpers/bash_syntax_checker.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+export GREEN='\033[0;32m'
+export ORANGE='\033[0;33m'
+export RED='\033[0;31m'
+export BLUE='\033[0;34m'
+export BOLD='\033[1m'
+export NC='\033[0m' # no color
+
+export EMBA_SOURCES_ARR=()
+EMBA_SOURCE_FILE=""
+
+import_emba_scripts() {
+  local lFILES_ARR=()
+  local lEMBA_FILE=""
+
+  mapfile -t lFILES_ARR < <(find ./ -type f -not \( -path "*/.git/*" -o -path "*/external/*" \) -prune 2>/dev/null)
+  for lEMBA_FILE in "${lFILES_ARR[@]}"; do
+    if (file "${lEMBA_FILE}" | grep -q "shell script"); then
+      echo "${lEMBA_FILE}"
+      EMBA_SOURCES_ARR+=("${lEMBA_FILE}")
+    fi
+  done
+}
+
+echo -e "\\n${ORANGE}${BOLD}Embedded Linux Analyzer Bash syntax checker${NC}"
+echo -e "${BOLD}=================================================================${NC}"
+
+echo -e "\\n${GREEN}Load all sources to check:${NC}\\n"
+import_emba_scripts
+
+echo -e "\\n${GREEN}Check all source for correct bash syntax:${NC}\\n"
+for EMBA_SOURCE_FILE in "${EMBA_SOURCES_ARR[@]}"; do
+  [[ ! -f "${EMBA_SOURCE_FILE}" ]] && continue
+  echo -e "\\n${GREEN}Run ${ORANGE}bash -n${GREEN} on ${ORANGE}${EMBA_SOURCE_FILE}${NC}\\n"
+  if [[ $(bash -n "${EMBA_SOURCE_FILE}") -eq 0 ]]; then
+    echo -e "${GREEN}${BOLD}==> SUCCESS${NC}\\n"
+  else
+    echo -e "\\n${ORANGE}${BOLD}==> FIX ERRORS${NC}\\n"
+    MODULES_TO_CHECK_ARR+=("${SOURCE}")
+  fi
+done
+
+if [[ "${#MODULES_TO_CHECK_ARR[@]}" -gt 0 ]]; then
+  echo "${RED}[-] WARNING: Syntax errors detected -> Fix before pushing to EMBA repo${NC}"
+  exit 1
+fi
+exit 0
